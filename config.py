44ed4fd6807308694c050c651c27ea42c2f21bd8
chauhan smit <chauhansmit39@gmail.com>
2026-06-27 17:09:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,1 @@
-ELEVENLABS_API_KEY = "sk_6279baa7c9f57706120c4fe61b87d9cf0d1b180bb903784f39"
-# Add your api_key[it's not work].it's free for all.Go to Elevenlabs website->Login->Generate your key.
+ELEVENLABS_API_KEY = "sk_6279baa7c9f57706120c4fe61b87d9cf0d1b180bb903784f"
